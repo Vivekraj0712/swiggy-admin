43292c7eeeb644cecacb993454b2284408002942
Vishal Nath Chauhan <63584407+vishal-nath-chauhan@users.noreply.github.com>
2022-11-22 22:14:35 +0530

    fixes

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,13 +1,17 @@
 const Data = require('../Data.json');
+
+// const data = window.localStorage.getItem('userData') ? JSON.parse(window.localStorage.getItem('userData')) : {};
+// const initialState = { courseData: data };
+
 const initialState = { courseData: Data };
 
 const reducer = (state = initialState, action) => {
-	// console.log({state});
 	switch (action.type) {
 		case 'FIND_SUBJECTS': {
 			let subjectsFilter = state.courseData.find((item) => item.Standard === Number(action.payload.standard));
 			subjectsFilter = subjectsFilter ? subjectsFilter.subjects : [];
 			let newState = { ...state, currentSubjects: subjectsFilter, currentStandard: action.payload.standard };
+			// window.localStorage.setItem('userData', JSON.stringify(newState));
 			return newState;
 		}
 
@@ -15,6 +19,7 @@ const reducer = (state = initialState, action) => {
 			let currentChapters = state.currentSubjects.find((chap) => chap.id === Number(action.payload.subjectId));
 			currentChapters = currentChapters ? currentChapters.Chapters : [];
 			let newState = { ...state, currentChapters };
+			// window.localStorage.setItem('userData', JSON.stringify(newState));
 			console.log(newState);
 			return newState;
 		}
